18357dd12330ba0aae7a7cbe077ccce4786d55dc
JakeLin <jakelin0fly@gmail.com>
2019-10-16 13:49:29 +0800

    新增样式表，空输入

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    QFile file(":/qss/qss/styleSheet.qss");
+    // 只读方式打开该文件
+    file.open(QFile::ReadOnly);
+    // 读取文件全部内容，使用tr()函数将其转换为QString类型
+    QString styleSheet = QString(file.readAll());
+    //QString styleSheet = QLatin1String(file.readAll());   //使用这种方式也可以
+    // 为QApplication设置样式表
+    qApp->setStyleSheet(styleSheet);
+
     w.show();
 
     return a.exec();
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,9 +9,13 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     setFixedSize(550, 350);
 
-    ui->title_lable->setText(QString("容斥原理-重集的r-组合"));
+    ui->title_label->setText(QString("容斥原理-重集的r-组合"));
+    ui->combination_lineEdit->setPlaceholderText(QString("集合序列"));
     ui->combination_label->setText(QString("集合序列,eg: k1*a1,k2*a2,k3*a3,..."));
-    ui->r_lable->setText(QString("组合数r"));
+
+    ui->r_lineEdit->setPlaceholderText(QString("r"));
+    ui->r_label->setText(QString("组合数r"));
+
     ui->answer->setText(QString(""));
 
     connect(ui->calculate, SIGNAL(clicked()), this, SLOT(onClickedSubmit()));
@@ -28,16 +32,21 @@ void MainWindow::onClickedSubmit(){
     QString rString = ui->r_lineEdit->text();
     foreach (QChar c, rString) {
         if(c < '0' || c > '9'){
-            QMessageBox::warning(this, QString("错误"), QString("数字r输入错误！请重新输入."),QMessageBox::Ok);
+            QMessageBox::warning(this, QString("错误"), QString("数字r输入错误！请重新输入."),
+                                 QMessageBox::Critical, QMessageBox::Ok);
             return;
         }
     }
-    myObj = new RCombination(ui->combination_lineEdit->text(), rString);
+    QString comString = ui->combination_lineEdit->text();
+    if(comString.isEmpty() || rString.isEmpty())
+        return;
+    myObj = new RCombination(comString, rString);
     if(-1 == myObj->getAnswer()){
-        QMessageBox::warning(this, QString("错误"), QString("集合字符串输入错误！请重新输入."),QMessageBox::Ok);
+        QMessageBox::warning(this, QString("错误"), QString("集合字符串输入错误！请重新输入."),
+                             QMessageBox::Critical, QMessageBox::Ok);
     }else {
         long long sum = myObj->calculate();
-        //    ui->answer->setText();
+            ui->answer->setText(QString::number(sum));
     }
 
     delete myObj;
